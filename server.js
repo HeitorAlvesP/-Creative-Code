70d6c3e529cebe687fc2d0ec4a738ffe1193a87f
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-09-09 15:46:35 -0300

    config inicial

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import connectDB from './conexao.js';
-const app = express();
 connectDB();
 
+const app = express();
+app.use(express.json())
+
+const users = [];
 
 
+app.post('/usuarios', (req, res) => {
+    users.push(req.body)
+    res.status(201).send('deu bom')
+});
 app.get('/usuarios', (req, res) => {
-    res.send('Okay, deu bom')
+    res.status(200).json(users)
 });
 
 
 app.listen(3000, () => {
     console.log('Server Rodando em porta 3000')
 });
-
-
