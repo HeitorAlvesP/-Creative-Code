78e5d6933e18a79f32ae411d2d47175bc9307062
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-09-10 15:51:06 -0300

    chega por hj

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ connectDB();
 app.use(express.json())
 app.use(express.static('public'));
 
-const users = [];
 
 app.post('/usuarios', async (req, res) => {
     try {
       const newUser = await prisma.users.create({  
         data: {
-          email: req.body.email,
+          email: req.body.email, //o email n pode ser repitido
           nome: req.body.nome,
           password: req.body.password
         }
@@ -26,13 +25,29 @@ app.post('/usuarios', async (req, res) => {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-})
-
-app.get('/usuarios', (req, res) => {
-    res.status(200).redirect('home.html')
 });
 
+app.put('/usuarios', async (req, res) => {
+      const newUser = await prisma.users.update({  
+        data: {
+          email: req.body.email, //o email n pode ser repitido
+          nome: req.body.nome,
+          password: req.body.password
+        }
+      });
+      res.status(201).json(newUser);
+});
+
+app.get('/usuarios', async (req, res) => {
+    const users = await prisma.users.findMany();
+
+    res.status(200).json(users);
+});
+
+app.get('/home', (req, res) => {
+    res.status(200).redirect('home.html');
+});
 
 app.listen(3000, () => {
-    console.log('Server Rodando em porta 3000')
+    console.log('Server Rodando em porta 3000');
 });
