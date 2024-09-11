a0391a6fd5b2c46aceed1e7eb46eef19f3e3a931
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-09-11 15:45:41 -0300

    Deus por favor faz rodar

--- server.js
+++ server.js
@@ -7,19 +7,38 @@ const app = express(); //express para fazer as rotas
 import connectDB from './static/conexao.js';
 connectDB(); //conexão do banco
 
-app.use(express.json())
+//import ValidaCPF from './static/valida_cpf.js';
+
+app.use(express.json()) //para ler json
+app.use(express.urlencoded({ extended: true })) // para ler o html
 app.use(express.static('public')); //para usar seus aqruivos no front
 
+app.get('/home', (req, res) => { //pagin inicial
+  res.status(200).redirect('home.html');
+});
 
-app.post('/usuarios', async (req, res) => {  //essa serve para criar
+app.post('/criar_conta', async (req, res) => {  //essa serve para criar
     try {
+      const {email, nome, password, confirm_password} = req.body
+
+      if (password !== confirm_password){
+        return res.send.status(400).json({ error: "As senhas não coincidem" })
+      }
+
+      // let validaCpf = new ValidaCPF(cpf)
+      // if(!validaCpf.valida()){
+      //   return res.send.status(400).json({ error: "CPF informado é invalido" })
+      // }
+
       const newUser = await prisma.users.create({  
         data: {
-          email: req.body.email, //o email n pode ser repitido
-          nome: req.body.nome,
-          password: req.body.password
+          //cpf,    // o cpf n pode ser repitido
+          email, //o email n pode ser repitido
+          nome,
+          password,
         }
       });
+
       res.status(201).json(newUser);
     } catch (error) {
       console.error(error);
@@ -27,10 +46,49 @@ app.post('/usuarios', async (req, res) => {  //essa serve para criar
     }
 });  //rota para criar
 
-app.put('/usuarios', async (req, res) => {  //editar um user esxistente
-      const newUser = await prisma.users.update({ //filrando pelo id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.put('/usuarios/:id', async (req, res) => {  //editar um user esxistente
+      const newUser = await prisma.user.update({ //filrando pelo id
+        where: {
+            id: req.params.id
+        },
         data: {
-          email: req.body.email, //o email n pode ser repitido
+          cpf: req.body.cpf,
+          email: req.body.email,
           nome: req.body.nome,
           password: req.body.password
         }
@@ -38,15 +96,22 @@ app.put('/usuarios', async (req, res) => {  //editar um user esxistente
       res.status(201).json(newUser);
 });
 
+app.delete('/usuarios/:id', async (req, res) => { //para deletar
+  await prisma.users.delete({
+    where: {
+      id: req.params.id //pegue i id como parametro na propria rota
+    }
+  }) 
+  res.status(200).json({ "messagem": "Usuario deletado com sucesso" })
+})
+
 app.get('/usuarios', async (req, res) => {          //aqui serve
     const users = await prisma.users.findMany();    //para exibir
                                                     //usuarios 
     res.status(200).json(users);                    //já cadastrados      
 });
 
-app.get('/home', (req, res) => { //pagin inicial
-    res.status(200).redirect('home.html');
-});
+
 
 app.listen(3000, () => {  //porta usado para rodar o sistema
     console.log('Server Rodando em porta 3000'); // sem precisar de servidor
