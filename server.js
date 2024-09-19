86df5fa1f1faf8afbc7f4d2c8c652d2ae6598a74
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-09-19 13:44:05 -0300

    PORRA EU CONSEGUI KRL

--- server.js
+++ server.js
@@ -33,19 +33,23 @@ app.get('/adm-menu', authMiddleware, (req, res) => {
   res.redirect(302, 'adm_menu.html');
 });
 
-app.get('/user/status', (req, res) => {
+app.get('/user/status', async (req, res) => {
   if (!req.session.userId) {
     return res.status(401).json({ message: 'Usuário não autenticado' });
   }
 
+  try {
+    const user = await User.findById(req.session.userId);
 
-  User.findById(req.session.userId, (err, user) => {
-    if (err || !user) {
-      return res.status(500).json({ message: 'Erro ao buscar o usuário' });
+    if (!user) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
     }
+
     res.json({ adm: user.adm });
-  });
-})
+  } catch (err) {
+    return res.status(500).json({ message: 'Erro ao buscar o usuário', error: err.message });
+  }
+});
 
 
 app.post('/login', realiza_login);
